1a79715e4f0b9ff16b654d417a9bb1eba47abd8d
Nicolas Ibarra <niicoibarra1@gmail.com>
2023-08-11 00:30:32 -0300

    Addition of schemas to Swagger

--- docs/swagger.js
+++ docs/swagger.js
@@ -8,7 +8,7 @@ const swaggerDefinition = {
   openapi: "3.1.0",
   info: {
     title: "REST API ",
-    description: "Hi there, this is the first REST API using Swagger I've ever created.",
+    description: "Hi there, this is the first REST API using Swagger and NodeJS I've ever created.",
     version: "1.0.0",
   },
   servers: [
@@ -16,15 +16,132 @@ const swaggerDefinition = {
       url: "http://localhost:3001/api",
     },
   ],
+  components:{
+    schemas:{
+      tracks:{
+        type:"object",
+        required:["name","artist"],
+        properties:{
+          name:{
+            type:"string"
+          },
+          album:{
+            type:"string"
+          },
+          cover:{
+            type:"string"
+          },
+          artist:{
+            type:"object",
+            required:["name"],
+            properties:{
+              name:{
+                type:"string"
+              },
+              nickname:{
+                type:"string"
+              },
+              nationality:{
+                type:"string"
+              }
+            }
+          },
+          duration:{
+            type:"object",
+            properties:{
+              start:{
+                type:"integer"
+              },
+              end:{
+                type:"integer"
+              }
+            }
+          },
+          mediaId:{
+            type:"string"
+          }
+        }
+      },
+      storage:{
+        type:"object",
+        required:["url"],
+        properties:{
+          url:{
+            type:"string"
+          },
+          filename:{
+            type:"string"
+          }
+        }
+      },
+      users:{
+        type:"object",
+        required:["name","age","email","password"],
+        properties:{
+          name:{
+            type:"string"
+          },
+          age:{
+            type:"integer"
+          },
+          email:{
+            type:"string"
+          },
+          password:{
+            type:"string"
+          },
+          role:{
+            type:"object",
+            required:[""],
+            properties:{
+              user:{
+                type:"string"
+              },
+              admin:{
+                type:"string"
+              }
+            }
+          }
+        }
+      },
+      authLogin:{
+        type:"object",
+        required:["email","password"],
+        properties:{
+          email:{
+            type:"string"
+          },
+          password:{
+            type:"string"
+          }
+        }
+      },
+      authRegister:{
+        type:"object",
+        required:["name","age","email","password"],
+        properties:{
+          name:{
+            type:"string"
+          },
+          age:{
+            type:"integer"
+          },
+          email:{
+            type:"string"
+          },
+          password:{
+            type:"string"
+          }
+        }
+      }
+    }
+  }
 };
 
 /**
- * The options constante has all the information that will be used to
- * create the Swagger documentation.
- * options.swaggerDefinition -> Establishes API information, like title,
- *                           -> or description.
- * options.apis -> Contains the path of the routes of the project and
- *              -> looks for every .js file.
+ * The constant "options" has all the information that will be used to create the Swagger documentation.
+ * options.swaggerDefinition -> Establishes API information, like title or description.
+ * options.apis -> Contains the path of the routes of the project and looks for every .js file.
  */
 const options = {
   swaggerDefinition,
